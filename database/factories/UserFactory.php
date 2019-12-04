d8516ba35ef7bff4db4bb8d17e98796d5df4ac40
saurav mitra <saurav.karate@gmail.com>
2019-12-04 14:51:51 +0800

    avatar_seed_fix #10

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,7 @@ $factory->define(User::class, function (Faker $faker) {
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
         'role' => 'guest',
-        'avatar' => 'https://source.unsplash.com/400x400',
+        'avatar' => 'https://i.pravatar.cc/100',
         'about_me' => $faker->paragraph(),
         'gender' => $faker->randomElement($array = [true, false]),
         'dob' => $faker->date($format = 'Y-m-d', $max = 'now'),
