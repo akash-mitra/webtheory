149068b3e735568522498c5b6a5f16d13afffc9d
Akash Mitra <akashmitra@gmail.com>
2020-07-02 11:43:08 +0800

    ability to add custom error pages

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,8 @@
+{{--
+    This blade file is called by Laravel when error is encountered.
+    We will first check if there is a custom page specific to
+    this error already present in the "active" directory. If
+    yes, we will load the custom page. If not, we will load
+    the default Laravel provided error page.
+--}}
+@includeFirst(['active.503', 'errors.default-503'])
