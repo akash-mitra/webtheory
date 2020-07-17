0654d400bdd84f5240db1620aa4d4b9e7ef1be4b
Akash Mitra <akashmitra@gmail.com>
2020-07-17 21:04:00 +0800

    release candidate

--- resources/js/ui/TensorToggle.vue
+++ resources/js/ui/TensorToggle.vue
@@ -1,73 +1,75 @@
 <template>
-
     <div @click="toggle" class="flex items-center cursor-pointer">
-
-        <div class="flex items-center" :class="boxClass + (value === trueValue ? ' justify-end' : ' justify-start')">
+        <div
+            class="flex items-center"
+            :class="boxClass + (value === trueValue ? ' justify-end' : ' justify-start')"
+        >
             <div :class="value === trueValue ? trueClass : falseClass">
-                <span v-if="showValue">{{ value }}</span>
+                <span v-if="showValue">{{
+                    value === trueValue ? trueLabel || value : falseLabel || value
+                }}</span>
             </div>
         </div>
 
         <slot></slot>
     </div>
-
 </template>
 
 <script>
-
 export default {
-
     props: {
         value: {
-
             type: String,
-            default: 'yes'
+            default: 'yes',
         },
 
         trueValue: {
             type: String,
-            default: 'yes'
+            default: 'yes',
         },
 
         falseValue: {
             type: String,
-            default: 'no'
+            default: 'no',
+        },
+
+        trueLabel: {
+            type: String,
+            default: null,
+        },
+
+        falseLabel: {
+            type: String,
+            default: null,
         },
 
         boxClass: {
             type: String,
-            default: 'w-12 h-4 bg-gray-200 border border-blue-200 rounded-lg'
+            default: 'w-12 h-4 bg-gray-200 border border-blue-200 rounded-lg',
         },
 
         trueClass: {
             type: String,
-            default: 'rounded-full h-4 w-4 shadow bg-blue-400'
+            default: 'rounded-full h-4 w-4 shadow bg-blue-400',
         },
 
         falseClass: {
             type: String,
-            default: 'rounded-full h-4 w-4 shadow bg-gray-400'
+            default: 'rounded-full h-4 w-4 shadow bg-gray-400',
         },
 
         showValue: {
             type: Boolean,
-            value: false
+            value: false,
         },
-
     },
-
 
     methods: {
-
         toggle() {
-
             let newValue = this.value === this.trueValue ? this.falseValue : this.trueValue
 
-            this.$emit("input", newValue);
-
-        }
+            this.$emit('input', newValue)
+        },
     },
-
 }
-
 </script>
