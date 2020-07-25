f8853afe68ab454096adf14bae2061f94fbe415d
Akash Mitra <akashmitra@gmail.com>
2020-07-25 13:14:29 +0800

    release candidate

--- resources/js/components/BoardTile.vue
+++ resources/js/components/BoardTile.vue
@@ -1,14 +1,17 @@
 <template>
     <div class="w-1/2 sm:w-1/4 xl:w-1/2 p-2 lg:p-3">
-        <div :class="tileClass + ' bg-' + tileColor + '-500'">
-            <div :class="metricClass + ' text-white'">
+        <div class="rounded-lg shadow px-6 py-4" :class="tileClass">
+            <div
+                class="text-3xl sm:text-4xl lg:text-5xl xl:text-5xl font-bold text-right"
+                :class="metricClass"
+            >
                 <slot name="metric"></slot>
             </div>
             <div class="">
-                <div :class="headerClass + ' text-' + tileColor + '-100'">
+                <div class="text-normal md:text-lg xl:text-2xl text-right" :class="textClass">
                     <slot name="header"></slot>
                 </div>
-                <div :class="textClass + ' text-' + tileColor + '-200'">
+                <div class="text-sm md:text-normal xl:text-lg text-right" :class="ctaClass">
                     <slot name="text"></slot>
                 </div>
             </div>
@@ -18,25 +21,21 @@
 <script>
 export default {
     props: {
-        tileColor: {
-            type: String,
-            default: 'blue',
-        },
         tileClass: {
             type: String,
-            default: 'rounded-lg shadow px-6 py-4',
+            default: 'bg-blue-500',
         },
         metricClass: {
             type: String,
-            default: 'text-3xl sm:text-4xl lg:text-5xl xl:text-5xl font-bold text-right',
-        },
-        headerClass: {
-            type: String,
-            default: 'text-normal md:text-lg xl:text-2xl text-right',
+            default: 'text-white',
         },
         textClass: {
             type: String,
-            default: 'text-sm md:text-normal xl:text-lg text-right',
+            default: 'text-blue-100',
+        },
+        ctaClass: {
+            type: String,
+            default: 'text-blue-200',
         },
     },
 }
