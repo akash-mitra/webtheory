fd7996e317affe91aa2015bce7842cfe5eb73681
saurav mitra <saurav.karate@gmail.com>
2020-07-25 13:34:00 +0800

    formResponseSeed

--- database/factories/FormResponseFactory.php
+++ database/factories/FormResponseFactory.php
@@ -6,9 +6,18 @@ use App\FormResponse;
 use Faker\Generator as Faker;
 
 $factory->define(FormResponse::class, function (Faker $faker) {
+    $formresponses = [
+        'name' => $faker->name,
+        'email' => $faker->email,
+        'address' => $faker->streetAddress,
+        'magic_house' => $faker->randomElement($array = ['Hufflepuff', 'Ravenclaw', 'Gryffindor', 'Slytherin']),
+        'mentor' => $faker->randomElement($array = ['Dumbledor', 'Severus Snape', 'Remus Lupin', 'Mad-eye Moody']),
+        'friends' => $faker->randomElement($array = ['Ron Weasley', 'Hermione', 'Neville Longbottom', 'Lucious Malfoy']),
+    ];
+
     return [
         // 'form_id' => 1,
         'ip' => $faker->ipv4,
-        'responses' => '["' . $faker->name . '","' . $faker->email . '"]',
+        'responses' => json_encode($formresponses),
     ];
 });
