19297154bf074361fe97a817a6bb540dde923bbc
Akash <akashmitra@gmail.com>
2021-02-16 17:03:54 +0800

    #101 New file type icons

---
+++ resources/js/components/icons/IconArchive.vue
@@ -0,0 +1,8 @@
+<template functional>
+    <svg class="icon icon-archive" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+         stroke-width="0.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <polyline points="21 8 21 21 3 21 3 8"></polyline>
+        <rect height="5" width="22" x="1" y="3"></rect>
+        <line x1="10" x2="14" y1="12" y2="12"></line>
+    </svg>
+</template>
